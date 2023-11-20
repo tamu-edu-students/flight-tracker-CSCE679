b524365849a22a29e839d6680dd80fb510b50ebe
Venkatakrishnan Sutharsan <venkatakrishnan.sutharsan@gmail.com>
2023-11-20 14:52:43 -0600

    Webpage Inital Development

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, session
+from flask import Flask, jsonify, request, session, render_template
 import live
 
 # live.get_languages()
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET']) #, methods=['POST'])
 def homepage():
-    return "Home Page!!"
+    return render_template("home.html")
 
 @app.route('/login') #, methods=['POST'])
 def login():
@@ -15,6 +15,11 @@ def login():
 @app.route('/language') #, methods=['POST'])
 def language():
     return jsonify(live.get_languages())
+
+
+@app.route('/getflight') #, methods=['POST'])
+def flight():
+    return jsonify(live.query_flight())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
--- live/__init__.py
+++ live/__init__.py
@@ -1,14 +1,29 @@
 import requests
 
-url = "https://sky-scanner3.p.rapidapi.com/languages"
+languages_url = "https://sky-scanner3.p.rapidapi.com/languages"
+flight_url = "https://sky-scrapper.p.rapidapi.com/api/v1/flights/getFlightDetails"
 
-headers = {
+skyscanner_headers = {
 	"X-RapidAPI-Key": "18607e0d16msh608c9f7e2ea52cep1ea2cfjsnfbdd1a2fce9c",
 	"X-RapidAPI-Host": "sky-scanner3.p.rapidapi.com"
 }
 
 def get_languages():
-	response = requests.get(url, headers=headers)
+	response = requests.get(languages_url, headers=skyscanner_headers)
 
 	print(response.json())
 	return response.json()
+
+
+def query_flight():
+
+	querystring = {"itineraryId":"135422402201235305980127122402201550","legs":"0","sessionId":"9eb73ffa206e4c268dbf6d03896eb584","adults":"1","currency":"USD"}
+
+	skyscrapper_headers = {
+		"X-RapidAPI-Key": "18607e0d16msh608c9f7e2ea52cep1ea2cfjsnfbdd1a2fce9c",
+		"X-RapidAPI-Host": "sky-scrapper.p.rapidapi.com"
+	}
+
+	response = requests.get(flight_url, headers=skyscrapper_headers, params=querystring)
+
+	print(response.json())
