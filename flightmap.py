174337512b371f561f26e8e80df4e1c169a4de4d
MagHaarseth <145481216+MagHaarseth@users.noreply.github.com>
2023-11-29 16:10:49 -0600

    Add files via upload

--- flightmap.py
+++ flightmap.py
@@ -18,20 +18,44 @@ scl.reverse()
 n_colors = len(scl)
 
 #New Data Frames filtered from user input
+
+
+
+
+# REPLACE THE CLEVELAND AND DENVER WITH WHATEVER THE USER PUTS IN
 userin = df[(df['city1'] == 'Cleveland')]
 userin2 = userin[(userin['city2'] == 'Denver')]
+
+lat1 = userin2['city 1 latitude'].iloc[1]
+lat2 = userin2['city 2 latitude'].iloc[1]
+lon1 = userin2['city 1 longitude'].iloc[1]
+lon2 = userin2['city 2 longitude'].iloc[1]
 userin3 = userin2[(userin2['quarter'] == 1)]
+
+
 print(userin3)
 
 
 
 
+#!!!!!!!!!!!!!!!!
 
+#THIS IS WHERE TO ADD THE DATAFRAME FROM THE API
+#Replace userin3 with dataframe from api
 
-
-fpaths = userin3[['city 1 longitude','city 2 longitude','city 1 latitude','city 2 latitude', 'fare']]
+fpaths = userin3[['airline','fare']]
 fpaths = fpaths.sort_values(by=['fare'])
-#fpaths.columns = ['city 1 longitude','city 2 longitude']
+
+
+#!!!!!!!!!!!!!!!!
+
+
+
+
+
+
+
+
 #MAP
 fpaths.head()
 print('!!!!!!!!!')
@@ -51,32 +75,22 @@ for i in range(len(fpaths)):
     if fpaths['fare'].iloc[i] == fpaths['fare'].min():
         var = 20
         clr = 'rgb(0,255,0)'
-    if fpaths['fare'].iloc[i] == fpaths['fare'].max() != fpaths['fare'].iloc[i] == fpaths['fare'].min():
+    if fpaths['fare'].iloc[i] == fpaths['fare'].max():
         clr = 'rgb(255,0,0)'
-    #r = 0
-    #g = 255
-    #if fpaths['fare'].iloc[i] < fpaths['fare'].max() and fpaths['fare'].iloc[i] > fpaths['fare'].min():
-        #r += (255/len(fpaths))*(i)
-        #r = str(r)
-        #print('r=')
-        #print(r)
-        #g -= (255/len(fpaths))*(i)
-        #g = str(g)
-        #print('g=')
-        #print(g)
-        #clr = 'rgb(' + r + ',' + g + ',0)'
+    if len(fpaths) == 1:
+        clr = 'rgb(0,255,0)'
     
     fig.add_trace(
         go.Scattergeo(
             locationmode = 'USA-states',
-            lat = [fpaths['city 1 latitude'].iloc[i]+(i), fpaths['city 2 latitude'].iloc[i]+(i)],
-            lon = [fpaths['city 1 longitude'].iloc[i], fpaths['city 2 longitude'].iloc[i]],
+            lat = [lat1+(i), lat2+(i)],
+            lon = [lon1, lon2],
             hoverinfo = 'text',
-            text = fpaths['fare'].iloc[i],
+            text = str(fpaths['fare'].iloc[i])+ ' , '  + fpaths['airline'],
+            hoverlabel = dict(bgcolor=clr,font_color='black'),
             mode = 'lines',
             line = dict(width = var,color = clr),
-            opacity = 1.0 #/ (float(fpaths['fare'].iloc[i])/100) ,
-            
+            opacity = 1.0
         )
     )
 for i in range(len(fpaths)):
@@ -85,18 +99,21 @@ for i in range(len(fpaths)):
         clr = 'rgb(0,255,0)'
     if fpaths['fare'].iloc[i] == fpaths['fare'].max():
         clr = 'rgb(255,0,0)'
+    if len(fpaths) == 1:
+        clr = 'rgb(0,255,0)'
     fig.add_trace(
         go.Scattergeo(
             locationmode = 'USA-states',
-            lat = [fpaths['city 1 latitude'].iloc[i]+(i), fpaths['city 2 latitude'].iloc[i]+(i)],
-            lon = [fpaths['city 1 longitude'].iloc[i], fpaths['city 2 longitude'].iloc[i]],
+            lat = [lat1+(i), lat2+(i)],
+            lon = [lon1, lon2],
             hoverinfo = 'text',
-            text = fpaths['fare'].iloc[i],
+            text = str(fpaths['fare'].iloc[i])+ ' , ' + fpaths['airline'],
+            hoverlabel = dict(bgcolor=clr,font_color='black'),
             mode = 'markers',
             marker = dict(
                 size = 10,
                 line = dict(width = 10,color = clr),
-                opacity = 1.0 #/ (float(fpaths['fare'].iloc[i])/100) ,
+                opacity = 1.0 
             
         )
     ))
@@ -105,10 +122,10 @@ print('did it get here !!!!!!!!!!!')
 #city one markers
 fig.add_trace(go.Scattergeo(
     locationmode = 'USA-states',
-    lon = userin3['city 1 longitude'],
-    lat = userin3['city 1 latitude'],
-    #hoverinfo = 'text',
-    #text = userin2['city1'],
+    lon = userin2['city 1 longitude'],
+    lat = userin2['city 1 latitude'],
+    hoverinfo = 'text',
+    text = userin2['city1'],
     mode = 'markers',
     marker = dict(
         size = 1,
@@ -122,10 +139,10 @@ fig.add_trace(go.Scattergeo(
 #city two markers
 fig.add_trace(go.Scattergeo(
     locationmode = 'USA-states',
-    lon = userin3['city 2 longitude'],
-    lat = userin3['city 2 latitude'],
-    #hoverinfo = 'text',
-    #text = userin2['city2'],
+    lon = userin2['city 2 longitude'],
+    lat = userin2['city 2 latitude'],
+    hoverinfo = 'text',
+    text = userin2['city2'],
     mode = 'markers',
     marker = dict(
         size = 1,
@@ -139,7 +156,7 @@ fig.add_trace(go.Scattergeo(
 
 print('here')
 fig.update_layout(
-    #title_text = 'Feb. 2011 American Airline flight paths<br>(Hover for airport names)',
+
     showlegend = False,
     geo = dict(
         scope = 'north america',
