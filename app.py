65c080dfa935f472168094b2204ff56a9b8d129d
Venkatakrishnan Sutharsan <venkatakrishnan.sutharsan@gmail.com>
2023-11-29 23:23:32 -0600

    History Support and Name changes

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import linegraph
 import flightmap
 import pandas as pd
 
+history = []
 # live.get_languages()
 app = Flask(__name__)
 
@@ -29,6 +30,22 @@ def flight():
 def airportsearch():
     return jsonify(live.getNearestAirport())
 
+@app.route('/history') #, methods=['POST'])
+def history_tofind():
+    ip = request.remote_addr
+    print(ip)
+    print(history)
+    print(len(history))
+    history_to_send = []
+    for i in range(len(history)):
+        if history[i][6] == ip:
+            history_to_send.append(history[i])
+            print("Added")
+            if len(history_to_send) == 5:
+                break
+    
+    return jsonify(history_to_send, 200)
+
 
 # @app.route('/getflightprice') #, methods=['POST'])
 # def getflightprice():
@@ -41,6 +58,14 @@ def getdata():
     passengers = request.form['passengers']
     cabinClass = request.form['cabinClass']
     budget = request.form['budget']
+    tmp_history = []
+    tmp_history.append(src)
+    tmp_history.append(dst)
+    tmp_history.append(date)
+    tmp_history.append(passengers)
+    tmp_history.append(cabinClass)
+    tmp_history.append(budget)
+    tmp_history.append(request.remote_addr)
     image_name = linegraph.generate_linegraph(src, dst)
     srcSkyId, srcEntityId = live.searchAirport(src, 1)
     time.sleep(1)
@@ -58,6 +83,12 @@ def getdata():
         processed_data = pd.DataFrame(processed_data, columns=['city1', 'city2', 'time', 'airlines', 'fare'])
         print(processed_data)
         html_path = flightmap.generate_map_html(processed_data)
+
+    tmp_history.append(image_name)
+    tmp_history.append(html_path)
+    print(tmp_history)
+    history.append(tmp_history)
+    print(history)
     return jsonify({"historical_file":image_name, "map_path":html_path}, 200)
 
     
