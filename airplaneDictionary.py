cee606c60ec5922256fdffecd6ff2fe34344f6f7
Surya Krishnan <43358838+suryakriz@users.noreply.github.com>
2023-11-29 21:44:20 -0600

    Airplane dictionary

---
+++ airplaneDictionary.py
@@ -0,0 +1,50 @@
+city1Dict = {
+    "Albany, New York" : 'ALB',
+    "Albuquerque, New Mexico" : 'ABQ',
+    "Allentown, Pennsylvania" : 'ABE',
+    "Amarillo, Texas" : 'AMA',
+    "Appleton, Wisconsin" : 'ATW',
+    "Asheville, North Carolina" : 'AVL',
+    "Ashland, West Virginia" : 'RIC',
+    "Aspen, Colorado" : 'ASE',
+    "Atlanta" : 'ATL',
+    "Atlantic City, New Jersey" : 'ACY',
+    "Austin, Texas" : 'AUS',
+    "Bangor, Maine" : 'BGR',
+    "Belleville, Illinois" : 'BLV',
+    "Bellingham, Washington" : "BLI",
+    "Bend, Oregon" : 'RDM',
+    "Billings, Montana" : 'BIL',
+    "Birmingham, Alabama" : 'BHM',
+    "Bismarck/Mandan, ND" : 'BIS',
+    "Boise, Idaho" : 'BOI',
+    "Boston" : 'BNH'
+}
+
+city2Dict = {
+    "Atlanta" : 'ATL',
+    "Austin, Texas" : 'AUS',
+    "Boise, Idaho" : 'BOI',
+    "Boston" : 'BNH',
+    "Bozeman, Montana" : 'BZN',
+    "Buffalo, New York" : 'BUF',
+    "Charleston, South Carolina" : 'CHS',
+    "Charlotte, North Carolina" : 'CLT',
+    "Chicago" : 'MDW',
+    "Cincinnati" : 'CVG',
+    "Cleveland" : 'CLE',
+    "Colorado Springs, Colorado" : 'COS',
+    "Columbus, Ohio" : 'CMH',
+    "Dayton, Ohio" : 'DAY',
+    "Denver" : 'BJC',
+    "Des Moines, Iowa" : 'DSM',
+    "Detroit" : 'DTW',
+    "Eagle, Colorado" : 'EGE',
+    "El Paso, Texas" : 'ELP',
+    "Fargo, North Dakota" : 'FAR'
+
+}
+
+print("\nDictionary with the use of Integer Keys: ")
+print(city1Dict)
+print(city2Dict)
